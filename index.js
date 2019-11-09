6a9005a6ca8f7f24531fcfc68f283fe7a833e789
unknown <technosusovan@gmail.com>
2019-11-09 16:52:12 +0530

    Adding JavaScript Tutorials

---
+++ index.js
@@ -0,0 +1,2 @@
+// This is my first JavaScript Code!
+console.log('Hello World')
---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = 'Susovan';
+console.log(name);
+
+// Cannot be reserved keyword
+// Should be meaningful and descriptive
+// Cannot start with a number (1name)
+// Cannot contain a space or hypen(-)
+// Are case-sensitive
+
+let firstName = 'Susovan';
+let lastName = 'Dutta';
